d49832ed1e1db9ccf81728dd48c6f5a83a09eb80
Travis <travisformayor@users.noreply.github.com>
2024-04-28 17:33:19 +0000

    define random_num function

--- helpers.asm
+++ helpers.asm
@@ -8,3 +8,21 @@
 .model flat
 
 .code
+; === WORD random_num(max_range: WORD) ===
+; Description: 
+;   Generates a random number within a specified range
+;   Parameters: Maximum end of the range (min is 0)
+;   Return: Randomly selected number (within the range)
+; Registers:
+;   EAX - Maximum range for the random number
+;   EBX - Random number generated (return value)
+;   EDX - Temporary storage for return pointer
+random_num PROC
+    pop edx         ; Save the return address
+    pop eax         ; Save maximum range in EAX
+    push edx        ; Restore return address
+
+    ; Generate a random number and store in EBX
+
+    ret
+random_num ENDP
